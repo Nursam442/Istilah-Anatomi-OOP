d613c2af132b01dec3d14bcf8730f0751fe30087
Nursam442 <Nursamhalal90@gmail.com>
2021-12-01 02:09:38 +0700

    Perubahan File

--- src/cg/Kopi.java
+++ src/cg/Kopi.java
@@ -22,26 +22,26 @@ public class Kopi {
         kapten.CetakHasil();
         kapten.asalKopi = "Bandung";
         kapten.harga = 5000;
-         System.out.println("Asal Kopi         : "+kapten.asalKopi);
-         System.out.println("harga             : "+kapten.harga);
-         System.out.println("----------------------------");
+        System.out.println("Asal Kopi         : "+kapten.asalKopi);
+        System.out.println("harga             : "+kapten.harga);
+        System.out.println("----------------------------");
          
-         Kopi torabika = new Kopi();
-         torabika.namaKopi = "Torabika";
-         torabika.CetakHasil();
-         torabika.asalKopi = "Jakarta";
-         torabika.harga = 6000;
-         System.out.println("Asal Kopi         : "+torabika.asalKopi);
-         System.out.println("harga             : "+torabika.harga);
-         System.out.println("-----------------------");
+        Kopi torabika = new Kopi();
+        torabika.namaKopi = "Torabika";
+        torabika.CetakHasil();
+        torabika.asalKopi = "Jakarta";
+        torabika.harga = 6000;
+        System.out.println("Asal Kopi         : "+torabika.asalKopi);
+        System.out.println("harga             : "+torabika.harga);
+        System.out.println("-----------------------");
          
-         Kopi barista = new Kopi();
-         barista.namaKopi = "Barista";
-         barista.CetakHasil();
-         barista.asalKopi ="Palembang";
-         barista.harga = 40000;
-         System.out.println("Asal Kopi         : "+torabika.asalKopi);
-         System.out.println("harga             : "+torabika.harga);
+        Kopi barista = new Kopi();
+        barista.namaKopi = "Barista";
+        barista.CetakHasil();
+        barista.asalKopi ="Palembang";
+        barista.harga = 4000;
+        System.out.println("Asal Kopi         : "+barista.asalKopi);
+        System.out.println("harga             : "+barista.harga);
     }
  
     
--- src/cg/Program.java
+++
@@ -1,7 +0,0 @@
-package cg;
-
-public class Program {
-    public static void main(String[] args) {
-        
-    }
-}
